7af00100f884cc838ab2b792bee28de500a3d70b
fisher60 <wakefollower@gmail.com>
2025-04-17 20:23:31 -0500

    WIP: websocket with game update

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
+use std::time::SystemTime;
+
 use futures::SinkExt;
 use futures::StreamExt;
+use warp::filters::ws::Message;
 use warp::Filter;
 use warp::ws;
 
 
-async fn receive_message(websocket: warp::ws::WebSocket){
-    let (mut sender, mut receiver) = websocket.split();
+async fn handle_websocket_connection(websocket: warp::ws::WebSocket){
+    println!("User connected!");
+    //let (mut sender, mut receiver) = websocket.split();
+    let (mut sender, _) = websocket.split();
 
-    while let Some(response) = receiver.next().await {
-        let message = match response {
-            Ok(msg) => msg,
-            Err(_) => panic!("Recieved message caused an error"),
-        };
+    let tick_time_ms = 500;
 
-        match sender.send(message).await {
-            Ok(_) => continue,
-            Err(_) => panic!("Error! Oh no!"),
+    let mut last_tick_time = SystemTime::now();
+
+    let tick_update_data = "some data";
+
+    loop {
+        let time_since_last_tick = SystemTime::now().duration_since(last_tick_time).expect("Time went backwards").as_secs() * 1000;
+        if time_since_last_tick >= tick_time_ms {
+            match sender.send(Message::text(tick_update_data)).await {
+                Ok(_) => {
+                    last_tick_time = SystemTime::now();
+                },
+                Err(_) => {
+                    println!("Failed to send game update message, disconnecting websocket...");
+                    break;
+                }
+            }
         }
     }
 
@@ -24,14 +38,16 @@ async fn receive_message(websocket: warp::ws::WebSocket){
 
 #[tokio::main]
 async fn main() {
+    println!("Setting up websocket server...");
 
     let routes = warp::path("ws")
         // The `ws()` filter will prepare the Websocket handshake.
         .and(ws())
         .map(|ws: ws::Ws| {
             // And then our closure will be called when it completes...
-            ws.on_upgrade(receive_message)
+            ws.on_upgrade(move |socket| handle_websocket_connection(socket))
         });
 
+    println!("Serving websocket at ws://127.0.0.1:8000/ws");
     warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
