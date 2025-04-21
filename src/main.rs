c7460fb5f24c9405dc9b935c8056bf4f770676c3
fisher60 <wakefollower@gmail.com>
2025-04-21 01:05:25 -0500

    add login message flow and example structs for sending game messages

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,83 @@ use warp::ws;
 
 const TICK_TIME_MS: Duration = Duration::from_millis(500);
 
+#[derive(Debug, PartialEq)]
+enum MessageType {
+    GameStateUpdate,
+    Login,
+}
+
+impl MessageType {
+    fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "update" => Some(MessageType::GameStateUpdate),
+            "login" => Some(MessageType::Login),
+            _ => None,
+        }
+    }
+
+    fn as_str(&self) -> &'static str {
+        match self {
+            MessageType::GameStateUpdate => "update",
+            MessageType::Login => "login",
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Response {
+    message_type: MessageType,
+}
+
+impl Response {
+    fn new(message_type: MessageType) -> Self {
+        Response { message_type }
+    }
+}
+
 
 async fn handle_websocket_connection(websocket: warp::ws::WebSocket){
     println!("User connected!");
-    let (mut sender, _) = websocket.split();
+    let (mut sender, mut recv) = websocket.split();
+
+    while let Some(result) = recv.next().await {
+        let msg = match result {
+            Ok(msg) => msg,
+            Err(e) => {
+                eprintln!("websocket error: {}", e);
+                return;
+            }
+        };
+
+        let str_msg = match msg.to_str() {
+            Ok(str_msg) => str_msg,
+            Err(e) => {
+                eprintln!("Invalid login request: {:?}", e);
+                return;
+            }
+        };
+
+        if str_msg == "login" {
+            let resp_message = Message::text("login success!");
+            match sender.send(resp_message).await {
+                Ok(_) => break,
+                Err(_) => {
+                    println!("Failed to send login message, disconnecting websocket...");
+                    break;
+                }
+            }
+        }
+        else {
+            let resp_message = Message::text("You must login before continuing...");
+            match sender.send(resp_message).await {
+                Ok(_) => continue,
+                Err(_) => {
+                    println!("Failed to send login fail message, disconnecting websocket...");
+                    break;
+                }
+            }
+        }
+    }
 
     let tick_update_data = "some data";
 
