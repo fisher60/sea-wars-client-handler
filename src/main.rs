24a38db63b5e5f79994f17dd5b22936e3055e8cd
fisher60 <wakefollower@gmail.com>
2025-04-21 00:04:29 -0500

    use async sleep in loop to unblock thread

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,28 @@
-use std::time::SystemTime;
+use std::time::Duration;
 
 use futures::SinkExt;
 use futures::StreamExt;
+use tokio::time;
 use warp::filters::ws::Message;
 use warp::Filter;
 use warp::ws;
 
+const TICK_TIME_MS: Duration = Duration::from_millis(500);
+
 
 async fn handle_websocket_connection(websocket: warp::ws::WebSocket){
     println!("User connected!");
-    //let (mut sender, mut receiver) = websocket.split();
     let (mut sender, _) = websocket.split();
-
-    let tick_time_ms = 500;
-
-    let mut last_tick_time = SystemTime::now();
 
     let tick_update_data = "some data";
 
     loop {
-        let time_since_last_tick = SystemTime::now().duration_since(last_tick_time).expect("Time went backwards").as_secs() * 1000;
-        if time_since_last_tick >= tick_time_ms {
-            match sender.send(Message::text(tick_update_data)).await {
-                Ok(_) => {
-                    last_tick_time = SystemTime::now();
-                },
-                Err(_) => {
-                    println!("Failed to send game update message, disconnecting websocket...");
-                    break;
-                }
+        time::sleep(TICK_TIME_MS).await;
+        match sender.send(Message::text(tick_update_data)).await {
+            Ok(_) => continue,
+            Err(_) => {
+                println!("Failed to send game update message, disconnecting websocket...");
+                break;
             }
         }
     }
